66aac396c06e043a284c62e7bd93b0f05a662a40
christin999 <christin9.k@gmail.com>
2023-11-13 00:02:07 +0200

    commit 2

---
+++ README.md
@@ -0,0 +1,8 @@
+I worked on 3 Chat boxes : 
+o	The first one is for finding the top restaurants in the world
+o	The second one is for searching music depends on genre 
+o	The third one is for the weather depends on location
+
+I have made the specific choices of Chat boxes in order to provide useful and basic information through links to the user,  simplifying their lives.
+
+I would like the chat box to be capable of understanding every day needs of the user and to be able to make life easier. Chat box must be trained to make more complicated and specified actions.
--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,44 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+action_server_url = "http://localhost:5055/webhook"
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionSearchYouTube(Action):
+    def name(self) -> Text:
+        return "action_search_youtube"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("ActionSearchYouTube")
+        print(f"Extracted text: {tracker.latest_message['entities'][0]}")
+        genre = tracker.latest_message['entities'][0]['value']
+
+        print(f"Extracted genre text: {genre}")
+        # You can replace this with your actual logic to search YouTube for songs of the given genre
+        search_query = f"{genre} songs"
+        youtube_search_url = f"https://www.youtube.com/results?search_query={search_query}"
+
+        dispatcher.utter_message(text=f"Sure! I've searched YouTube for {genre} songs. [YouTube Search]({youtube_search_url})")
+
+        return []
+
+
+class ActionGetWeather(Action):
+    def name(self) -> Text:
+        return "action_search_weather"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("ActionGetWeather")
+        print(f"Extracted text: {tracker.latest_message['entities'][0]}")
+        location = tracker.latest_message['entities'][0]['value']
+
+        print(f"Extracted text: {tracker.latest_message['entities'][0]}")
+        print(f"Extracted genre text: {location}")
+        # You can replace this with your actual logic to search YouTube for songs of the given genre
+        search_query = location.replace(" ", "+")
+        weather_search_url = f"https://www.google.com/search?q=weather+in+{search_query}"
+
+        dispatcher.utter_message(text=f"Sure! I've searched Google for {location} weather. [Google Search]({weather_search_url})")
+
+        return []
